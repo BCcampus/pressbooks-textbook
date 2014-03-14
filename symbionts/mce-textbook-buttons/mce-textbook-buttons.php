2f9e210905cb6c45894f52f436815f46dc36140f
Brad <brad@bradpayne.ca>
2014-03-14 09:47:17 -0700

    Update mce-textbook-buttons.php

--- symbionts/mce-textbook-buttons/mce-textbook-buttons.php
+++ symbionts/mce-textbook-buttons/mce-textbook-buttons.php
@@ -84,8 +84,8 @@ class TextbookButtons {
 
 }
 // Display only if a textbook theme is active
-$t = wp_get_theme();
+$t = wp_get_theme()->Tags;
 
-if ( in_array( 'Pressbooks Textbook', $t->get( 'Tags' ) ) ) {
+if ( is_array( $t ) && in_array( 'Pressbooks Textbook', $t ) ) {
 	$textbook_buttons = new \PBT\Plugins\TextbookButtons();
 }
