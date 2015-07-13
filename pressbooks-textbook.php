6ba32c1ea70ee2543f10989f1099b9cae079e9c8
bdolor <bpayne@bccampus.ca>
2015-07-13 12:48:27 -0700

    updating version number

--- pressbooks-textbook.php
+++ pressbooks-textbook.php
@@ -11,7 +11,7 @@
  * @wordpress-plugin
  * Plugin Name:       PressBooks Textbook
  * Description:       A plugin that extends PressBooks for textbook authoring
- * Version:           1.2.10
+ * Version:           1.2.11
  * Author:            Brad Payne
  * Author URI:        http://bradpayne.ca		
  * Text Domain:       pressbooks-textbook
@@ -38,7 +38,7 @@ class Textbook {
 	 * @since 1.0.0
 	 * @var string
 	 */
-	const VERSION = '1.2.10';
+	const VERSION = '1.2.11';
 
 	/**
 	 * Unique identifier for plugin.
