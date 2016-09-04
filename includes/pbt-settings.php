3172f8c8897e5841463a385f4ea14b22e27f15c1
Ned Zimmerman <ned@bight.ca>
2016-09-04 16:36:14 -0300

    Remove all redistribution functionality from PBT.
    Full redistribution capability will be available in Pressbooks >= 3.8.
    After this PR, Pressbooks Textbook will enable redistribution by default
    as per #67.

--- includes/pbt-settings.php
+++ includes/pbt-settings.php
@@ -2,68 +2,19 @@
 
 /**
  * Options/Settings for Pressbooks Textbook plugin
- * 
+ *
  * @package Pressbooks_Textbook
  * @author Brad Payne <brad@bradpayne.ca>
  * @license   GPL-2.0+
- * 
+ *
  * @copyright 2014 Brad Payne
  */
 
 namespace PBT\Settings;
 
 /**
- * Provides a simple description for plugins that support redistribution
- * 
- * @since 1.0.2
- */
-function redistribute_section_callback() {
-	echo "<p>If they exist, one of each of the latest export files (epub, pdf, xhtml, hpub, mobi, wxr, icml) will be available for download on the homepage.</p>"
-	. '<figure><img src="' . PBT_PLUGIN_URL . 'admin/assets/img/latest-export-files.png" /><figcaption>The dowload links as they would appear on the homepage.</figcaption></figure>';
-}
-
-/**
- * Fields callback
- * 
- * @since 1.0.2
- * @param $args
- */
-function latest_files_public_callback( $args ) {
-	$options = get_option( 'pbt_redistribute_settings' );
-
-	// add default if not set
-	if ( ! isset( $options['latest_files_public'] ) ) {
-		$options['latest_files_public'] = 0;
-	}
-
-	$html = '<input type="radio" id="files-public" name="pbt_redistribute_settings[latest_files_public]" value="1"' . checked( 1, $options['latest_files_public'], false ) . '/> ';
-	$html .= '<label for="files-public"> ' . __( 'Yes. I would like the latest export files to be available on the homepage for free, to everyone.', 'pressbooks-textbook' ) . '</label><br />';
-	$html .= '<input type="radio" id="files-admin" name="pbt_redistribute_settings[latest_files_public]" value="0" ' . checked( 0, $options['latest_files_public'], false ) . '/> ';
-	$html .= '<label for="files-admin"> ' . __( 'No. I would like the latest export files to only be available to administrators. (Pressbooks default)', 'pressbooks-textbook' ) . '</label>';
-	echo $html;
-}
-
-/**
- * Redistribute Sanitization callback
- * 
- * @since 1.0.1
- * @param array $input
- * @return array
- */
-function redistribute_absint_sanitize( $input ) {
-	$options = get_option( 'pbt_redistribute_settings' );
-
-	// radio buttons
-	foreach ( array( 'latest_files_public' ) as $val ) {
-		$options[$val] = absint( $input[$val] );
-	}
-
-	return $options;
-}
-
-/**
  * Simple description for Other plugins that support textbooks
- * 
+ *
  * @since 1.0.2
  */
 function pbt_other_section_callback() {
@@ -72,7 +23,7 @@ function pbt_other_section_callback() {
 
 /**
  * Fields callback for Hypothesis
- * 
+ *
  * @since 1.0.2
  */
 function pbt_hypothesis_active_callback() {
@@ -92,7 +43,7 @@ function pbt_hypothesis_active_callback() {
 
 /**
  * Sanitization callback
- * 
+ *
  * @param array $input
  * @return array
  */
@@ -109,7 +60,7 @@ function other_absint_sanitize( $input ) {
 
 /**
  * Simple description for Creative Commons License
- * 
+ *
  * @since 1.0.2
  */
 function pbt_reuse_section_callback() {
@@ -139,7 +90,7 @@ function pbt_reuse_section_callback() {
 
 /**
  * Fields callback for Creative Commons
- * 
+ *
  * @since 1.0.2
  */
 function pbt_ccc_active_callback() {
@@ -150,21 +101,21 @@ function pbt_ccc_active_callback() {
 		$options['pbt_creative-commons-configurator-1_active'] = 0;
 	}
 
-	$html = "<p>The Creative Commons Configurator by George Notaras '<i>adds Creative Commons license information to your posts, pages, attachment pages and feeds.</i>'</p>";	
+	$html = "<p>The Creative Commons Configurator by George Notaras '<i>adds Creative Commons license information to your posts, pages, attachment pages and feeds.</i>'</p>";
 	$html .= '<input type="radio" id="ccc-active" name="pbt_reuse_settings[pbt_creative-commons-configurator-1_active]" value="1" ' . checked( 1, $options['pbt_creative-commons-configurator-1_active'], false ) . '/> ';
 	$reminder = (true == $options['pbt_creative-commons-configurator-1_active']) ? ' Make sure you <a href="options-general.php?page=cc-configurator-options">configure your license</a>.' : '';
 	$html .= '<label for="ccc-active"> ' . __( 'Yes. I would like to add Creative Commons license information to my book pages.', 'pressbooks-textbook' ) . $reminder . '</label><br />';
 	$html .= '<input type="radio" id="ccc-not-active" name="pbt_reuse_settings[pbt_creative-commons-configurator-1_active]" value="0" ' . checked( 0, $options['pbt_creative-commons-configurator-1_active'], false ) . '/> ';
 
 	$html .= '<label for="ccc-not-active"> ' . __( 'No. I would not like to add Creative Commons license information to my book pages.', 'pressbooks-textbook' ) . '</label>';
-	
+
 
 	echo $html;
 }
 
 /**
  * Sanitization callback
- * 
+ *
  * @since 1.0.2
  * @param array $input
  * @return array
@@ -181,20 +132,20 @@ function reuse_absint_sanitize( $input ) {
 }
 
 /**
- * 
+ *
  */
 function remix_section_callback(){
 	echo "<p>If you know of another Pressbooks instance, and you know they also have Creative Commons licensed materials, here is where you add their domain."
 	. " Having a list of domains will enable <a href='admin.php?page=api_search_import'>searching and importing</a> against their collection, the same way that you can search and import against your own collection.</p>";
-	
-} 
+
+}
 
 /**
- * 
+ *
  */
 function api_endpoint_public_callback() {
 	$options = get_option( 'pbt_remix_settings' );
-	
+
 	// add default if not set
 	if ( ! isset( $options['pbt_api_endpoints'] ) ) {
 		$options['pbt_api_endpoints'][0] = network_home_url();
@@ -221,7 +172,7 @@ function api_endpoint_public_callback() {
 }
 
 /**
- * 
+ *
  * @param type $input
  * @return type
  */
@@ -239,24 +190,24 @@ function remix_url_sanitize( $input ) {
 		// sanitize, reset the key to maintain sequential numbering, to account for blank entries
 		$valid['pbt_api_endpoints'][$i] = trailingslashit(esc_url( $url, $protocols )) ;
 
-		// check if they are API enabled Pressbooks instances: 
+		// check if they are API enabled Pressbooks instances:
 		$api_endpoint = $valid['pbt_api_endpoints'][$i] . 'api/v1/books/';
 
 		$check_response = wp_remote_head( $api_endpoint );
 		$code = wp_remote_retrieve_response_code($check_response);
-		
+
 		if ( $code > 400 ) {
 			$msg = $code . ' returned for ' . $api_endpoint . ' — the domain you tried to add did not return a response code we can not work with.';
 			add_settings_error(
-				'pbt_remix_settings', 
-				'settings_updated', 
-				$msg, 
+				'pbt_remix_settings',
+				'settings_updated',
+				$msg,
 				'error'
 			);
 			// jankified, so discard
 			unset( $valid['pbt_api_endpoints'][$i] );
 		}
-		
+
 		$i ++;
 	}
 
@@ -264,13 +215,13 @@ function remix_url_sanitize( $input ) {
 	if ( network_home_url() != $valid['pbt_api_endpoints'][0] ){
 		$valid['pbt_api_endpoints'][0] = network_home_url();
 	}
-	
+
 	add_settings_error(
 		'pbt_remix_settings',
 		'settings_updated',
 		$success_msg,
 		'updated'
 		);
-	
+
 	return $valid;
 }
