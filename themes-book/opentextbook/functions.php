46ec9cae8092cf7c794d2da8953d0d56412c9a0d
bdolor <bpayne@bccampus.ca>
2017-02-14 13:40:06 -0800

    reformatting functions.php

--- themes-book/opentextbook/functions.php
+++ themes-book/opentextbook/functions.php
@@ -1,32 +1,31 @@
 <?php
 
 /**
- * Returns an html blog of meta elements 
- * 
+ * Returns an html blog of meta elements
+ *
  * @return string $html metadata
  */
 function pbt_get_seo_meta_elements() {
 	// map items that are already captured
 	$meta_mapping = array(
 
-		'citation_title' => 'pb_title',
-		'citation_author' => 'pb_authors_file_as',
-		'citation_language' => 'pb_language',
-		'citation_keywords' => 'pb_keywords_tags',
-		'citation_pdf_url' => pbt_get_citation_pdf_url(),
+		'citation_title'            => 'pb_title',
+		'citation_author'           => 'pb_authors_file_as',
+		'citation_language'         => 'pb_language',
+		'citation_keywords'         => 'pb_keywords_tags',
+		'citation_pdf_url'          => pbt_get_citation_pdf_url(),
 		'citation_publication_date' => 'pb_publication_date',
 
 	);
 
-	$html = "<meta name='application-name' content='Pressbooks'>\n";
+	$html     = "<meta name='application-name' content='Pressbooks'>\n";
 	$metadata = \Pressbooks\Book::getBookInformation();
 
 	// create meta elements
 	foreach ( $meta_mapping as $name => $content ) {
 		if ( array_key_exists( $content, $metadata ) ) {
-			$html .= "<meta name='" . $name . "' content='" . $metadata[$content] . "'>\n";
-		}
-		elseif( 'citation_pdf_url' == $name ){
+			$html .= "<meta name='" . $name . "' content='" . $metadata[ $content ] . "'>\n";
+		} elseif ( 'citation_pdf_url' == $name ) {
 			$html .= "<meta name='" . $name . "' content='" . $content . "'>\n";
 		}
 	}
@@ -35,7 +34,7 @@ function pbt_get_seo_meta_elements() {
 }
 
 function pbt_get_citation_pdf_url() {
-	$url = '';
+	$url    = '';
 	$domain = site_url();
 
 	if ( method_exists( '\Pressbooks\Utility', 'latest_exports' ) ) {
@@ -53,6 +52,7 @@ function pbt_get_citation_pdf_url() {
 			}
 		}
 	}
+
 	return $url;
 }
 
@@ -62,69 +62,71 @@ function pbt_get_microdata_meta_elements() {
 
 	// add elements that aren't captured, and don't need user input
 	$edu_align = ( isset( $metadata['pb_bisac_subject'] ) ) ? $metadata['pb_bisac_subject'] : '';
-	
+
 	$lrmi_meta = array(
-	    'educationalAlignment' => $edu_align,
-	    'educationalUse' => 'Open textbook study',
-	    'audience' => 'student',
-	    'interactivityType' => 'mixed',
-	    'learningResourceType' => 'textbook',
-	    'typicalAgeRange' => '17-',
+		'educationalAlignment' => $edu_align,
+		'educationalUse'       => 'Open textbook study',
+		'audience'             => 'student',
+		'interactivityType'    => 'mixed',
+		'learningResourceType' => 'textbook',
+		'typicalAgeRange'      => '17-',
 	);
 
 	foreach ( $lrmi_meta as $itemprop => $content ) {
 		// @todo parse educationalAlignment items into alignmentOjects
 		$html .= "<meta itemprop='" . $itemprop . "' content='" . $content . "' id='" . $itemprop . "'>\n";
 	}
+
 	return $html;
 }
 
 /**
- * Modifies 'chapters' to 'page' for text processed in __() to avoid confusion. 
+ * Modifies 'chapters' to 'page' for text processed in __() to avoid confusion.
  * Lightly modified function, original author Lumen Learning
  * https://github.com/lumenlearning/candela
- * 
- * 
+ *
+ *
  * @param type $translated
  * @param type $original
  * @param type $domain
+ *
  * @return type
  */
 function pbt_terminology_modify( $translated, $original, $domain ) {
 
 	if ( 'pressbooks' == $domain ) {
 		$modify = array(
-		    "Chapter Metadata" => "Page Metadata",
-		    "Chapter Short Title (appears in the PDF running header)" => "Page Short Title (appears in the PDF running header)",
-		    "Chapter Subtitle (appears in the Web/ebook/PDF output)" => "Page Subtitle (appears in the Web/ebook/PDF output)",
-		    "Chapter Author (appears in Web/ebook/PDF output)" => "Page Author (appears in Web/ebook/PDF output)",
-		    "Chapter Copyright License (overrides book license on this page)" => "Page Copyright License (overrides book license on this page)",
-		    "Promote your book, set individual chapters privacy below." => "Promote your book, set individual page's privacy below.",
-		    "Add Chapter" => "Add Page",
-		    "Reordering the Chapters" => "Reordering the Pages",
-		    "Chapter 1" => "Page 1",
-		    "Imported %s chapters." => "Imported %s pages.",
-		    "Chapters" => "Pages",
-		    "Chapter" => "Page",
-		    "Add New Chapter" => "Add New Page",
-		    "Edit Chapter" => "Edit Page",
-		    "New Chapter" => "New Page",
-		    "View Chapter" => "View Page",
-		    "Search Chapters" => "Search Pages",
-		    "No chapters found" => "No pages found",
-		    "No chapters found in Trash" => "No pages found in Trash",
-		    "Chapter numbers" => "Page numbers",
-		    "display chapter numbers" => "display page numbers",
-		    "do not display chapter numbers" => "do not display page numbers",
-		    "Chapter Numbers" => "Page Numbers",
-		    "Display chapter numbers" => "Display page numbers",
-		    "This is the first chapter in the main body of the text. You can change the " => "This is the first page in the main body of the text. You can change the ",
-		    "text, rename the chapter, add new chapters, and add new parts." => "text, rename the page, add new pages, and add new parts.",
-		    "Only users you invite can see your book, regardless of individual chapter " => "Only users you invite can see your book, regardless of individual page ",
+			"Chapter Metadata"                                                            => "Page Metadata",
+			"Chapter Short Title (appears in the PDF running header)"                     => "Page Short Title (appears in the PDF running header)",
+			"Chapter Subtitle (appears in the Web/ebook/PDF output)"                      => "Page Subtitle (appears in the Web/ebook/PDF output)",
+			"Chapter Author (appears in Web/ebook/PDF output)"                            => "Page Author (appears in Web/ebook/PDF output)",
+			"Chapter Copyright License (overrides book license on this page)"             => "Page Copyright License (overrides book license on this page)",
+			"Promote your book, set individual chapters privacy below."                   => "Promote your book, set individual page's privacy below.",
+			"Add Chapter"                                                                 => "Add Page",
+			"Reordering the Chapters"                                                     => "Reordering the Pages",
+			"Chapter 1"                                                                   => "Page 1",
+			"Imported %s chapters."                                                       => "Imported %s pages.",
+			"Chapters"                                                                    => "Pages",
+			"Chapter"                                                                     => "Page",
+			"Add New Chapter"                                                             => "Add New Page",
+			"Edit Chapter"                                                                => "Edit Page",
+			"New Chapter"                                                                 => "New Page",
+			"View Chapter"                                                                => "View Page",
+			"Search Chapters"                                                             => "Search Pages",
+			"No chapters found"                                                           => "No pages found",
+			"No chapters found in Trash"                                                  => "No pages found in Trash",
+			"Chapter numbers"                                                             => "Page numbers",
+			"display chapter numbers"                                                     => "display page numbers",
+			"do not display chapter numbers"                                              => "do not display page numbers",
+			"Chapter Numbers"                                                             => "Page Numbers",
+			"Display chapter numbers"                                                     => "Display page numbers",
+			"This is the first chapter in the main body of the text. You can change the " => "This is the first page in the main body of the text. You can change the ",
+			"text, rename the chapter, add new chapters, and add new parts."              => "text, rename the page, add new pages, and add new parts.",
+			"Only users you invite can see your book, regardless of individual chapter "  => "Only users you invite can see your book, regardless of individual page ",
 		);
 
-		if ( isset( $modify[$original] ) ) {
-			$translated = $modify[$original];
+		if ( isset( $modify[ $original ] ) ) {
+			$translated = $modify[ $original ];
 		}
 	}
 
@@ -135,17 +137,19 @@ function pbt_terminology_modify( $translated, $original, $domain ) {
  * Modifies 'chapter' to 'page' for text processed in _x()
  * Lightly modified function, original author Lumen Learning
  * https://github.com/lumenlearning/candela
- * 
+ *
  * @param type $translated
  * @param type $original
  * @param type $context
  * @param type $domain
+ *
  * @return type
  */
 function pbt_terminology_modify_context( $translated, $original, $context, $domain ) {
 	if ( 'pressbooks' == $domain && 'book' == $context ) {
 		$translated = pbt_terminology_modify( $translated, $original, $domain );
 	}
+
 	return $translated;
 }
 
@@ -155,18 +159,20 @@ function pbt_terminology_modify_context( $translated, $original, $context, $doma
 
 // removes incorrect notice on epub/pdf export that the book was created on pressbooks.com
 $GLOBALS['PB_SECRET_SAUCE']['TURN_OFF_FREEBIE_NOTICES_EPUB'] = 'not_created_on_pb_com';
-$GLOBALS['PB_SECRET_SAUCE']['TURN_OFF_FREEBIE_NOTICES_PDF'] = 'not_created_on_pb_com';
+$GLOBALS['PB_SECRET_SAUCE']['TURN_OFF_FREEBIE_NOTICES_PDF']  = 'not_created_on_pb_com';
 
 
 /**
- * 
+ *
  * @staticvar array $searches
+ *
  * @param type $content
+ *
  * @return type
  */
 function pbt_fix_img_relative( $content ) {
 	static $searches = array(
-	    '#<(?:img) .*?src=[\'"]\Khttp://[^\'"]+#i', // fix image and iframe elements
+		'#<(?:img) .*?src=[\'"]\Khttp://[^\'"]+#i', // fix image and iframe elements
 	);
 	$content = preg_replace_callback( $searches, 'pbt_fix_img_relative_callback', $content );
 
@@ -174,19 +180,21 @@ function pbt_fix_img_relative( $content ) {
 }
 
 /**
- * 
+ *
  * @param type $matches
+ *
  * @return type
  */
-function pbt_fix_img_relative_callback($matches){
-	$avoid = 'http://s.wordpress.com';
+function pbt_fix_img_relative_callback( $matches ) {
+	$avoid    = 'http://s.wordpress.com';
 	$protocol = '';
 
-	if ( 0 === strcmp( $avoid, substr( $matches[0],0,22 ) ) ) {
+	if ( 0 === strcmp( $avoid, substr( $matches[0], 0, 22 ) ) ) {
 		$protocol = $matches[0];
 	} else {
 		$protocol = '' . substr( $matches[0], 5 );
 	}
+
 	return $protocol;
 }
 
@@ -197,9 +205,8 @@ if ( ! empty( $_SERVER['HTTPS'] ) ) {
 /**
  * adds metadata to head element
  */
-function pbt_add_metadata()
-{
-	if (is_front_page()) {
+function pbt_add_metadata() {
+	if ( is_front_page() ) {
 		echo pbt_get_seo_meta_elements();
 		echo pbt_get_microdata_meta_elements();
 	} else {
