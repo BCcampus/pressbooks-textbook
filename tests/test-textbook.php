091f79c71bbc81d61a67b9c8f64cb070d822822e
bdolor <bpayne@bccampus.ca>
2018-08-03 15:20:12 -0700

    update test to meet php standards

--- tests/test-textbook.php
+++ tests/test-textbook.php
@@ -3,12 +3,8 @@
 
 class TextbookTest extends WP_UnitTestCase {
 
-	protected $pbt;
-
 	public function setUp() {
 		parent::setUp();
-		$this->pbt = \PBT\Textbook::get_instance();
-
 	}
 
 	public function tearDown() {
@@ -19,17 +15,17 @@ class TextbookTest extends WP_UnitTestCase {
 		$this->assertInstanceOf( '\PBT\Textbook', $this->pbt );
 	}
 
-
 	function test_isTexbookTheme() {
 		register_theme_directory( __DIR__ . '/data/themes' );
+		$pbt = \PBT\Textbook::get_instance();
 
 		$nope = wp_get_theme( 'notpbt', __DIR__ . '/data/themes' );
-		$f    = $this->pbt::isTextbookTheme( $nope );
+		$f    = $pbt::isTextbookTheme( $nope );
 		$this->assertFalse( $f );
 		wp_clean_themes_cache();
 
 		$yup = wp_get_theme( 'pbt', __DIR__ . '/data/themes' );
-		$t   = $this->pbt::isTextbookTheme( $yup );
+		$t   = $pbt::isTextbookTheme( $yup );
 		$this->assertTrue( $t );
 		wp_clean_themes_cache();
 	}
